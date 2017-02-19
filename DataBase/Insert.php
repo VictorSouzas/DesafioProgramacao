3eded2b1392c3791c2538705e7f4a34711f80c0c
Victor Souza <victor_souzas@yahoo.com.br>
2017-02-19 10:04:32 -0300

    Inplememts InsertInstrucion
    Add methods setConlumsAsValue,
    createColuns, createValues and getInstruction

--- DataBase/Insert.php
+++ DataBase/Insert.php
@@ -1,9 +1,50 @@
 <?php
-	namespace \DataBase\Criteria;
-	class Insert extends Instruction{
+	namespace DataBase;
+	use DataBase\Instruction;
+	
+	class Insert extends Instruction {
 		
+		private $columns;
+		private $values;
+		private $valueStm;
+		private $columnStm;
 		
-		public function getInstruction(){
-
+		function __construct(){
+			$this->columns = array();
+			$this->values = array();
+			$this->valueStm = "";
+			$this->columnStm = "";
+		}
+		
+		public function setConlumnAsValue(string $column, string $value): void{
+			array_push($this->columns, $column);
+			array_push($this->values, $value);
+		}
+		
+		protected function createConlumns(): void {
+			$this->columnStm .= "(";
+			foreach ($this->columns as $column){
+				$this->columnStm .= "{$column},";
+			}
+			$this->columnStm = substr($this->columnStm, 0, -1);
+			$this->columnStm .= ")";
+		}
+		
+		protected function createValues(): void {
+			$this->valueStm .= "VALUES (";
+			foreach ($this->values as $value){
+				$this->valueStm .= "{$value},";
+			}
+			$this->valueStm = substr($this->valueStm, 0, -1);
+			$this->valueStm .= ")";
+		}
+		
+		public function getInstruction(): string{
+			self::createConlumns();
+			self::createValues();
+			$sqlStm = "INSERT INTO ";
+			$sqlStm .= parent::getEntity();
+			$sqlStm .= "{$this->columnStm} {$this->valueStm};";
+			return $sqlStm;
 		}
 	}
