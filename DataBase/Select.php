9eb2aeaf2585d8d46b0b917f98f599574db5324d
Victor Souza <victor_souzas@yahoo.com.br>
2017-02-19 13:14:45 -0300

    Select instruction

---
+++ DataBase/Select.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace DataBase;
+
+use DataBase\Instruction;
+class Select extends Instruction {
+	private $columns;
+	protected $columnStm;
+	private $criteria;
+	
+	function __construct(){
+		$this->columns = array();
+		$this->criteria = "";
+	}
+	public function setColumn(string $column): void {
+		array_push ( $this->columns, $column );
+	}
+	
+	protected function makeColumns(): void{
+		$this->columnStm = "";
+		foreach ($this->columns as $column){
+			$this->columnStm .= "{$column},";
+		}
+		$this->columnStm = substr($this->columnStm, 0, -1);
+	}
+	
+	public function setCriteria(string $criteria): void {
+		$this->criteria = $criteria;
+	}
+	
+	public function getInstruction(): string{
+		self::makeColumns();
+		$stm = "SELECT {$this->columnStm} FROM ";
+		$stm .= parent::getEntity();
+		if($this->criteria != ""){
+			$stm .= " {$this->criteria}";
+		}
+		$stm .= ";";
+		return $stm;
+		
+	}
+}
