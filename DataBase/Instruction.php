d3010a3a8d238c700e3935539b5f2f4d605260ba
Victor Souza <victor_souzas@yahoo.com.br>
2017-02-19 10:01:09 -0300

    Declare NameSpace
    DataBase

--- DataBase/Instruction.php
+++ DataBase/Instruction.php
@@ -1,10 +1,11 @@
 <?php
+namespace DataBase;
 	abstract class Instruction{
 		private $entity;
 		private $instruction;
 
 		public function setEntity(string $entity){
-			$this->entity = entity;
+			$this->entity = $entity;
 		}
 		public function getEntity() : string{
 			return $this->entity;
