830f07786603754f349618a29eed320174230003
Victor Souza <victor_souzas@yahoo.com.br>
2017-02-19 20:24:26 -0300

    Index
    Render Twig Template

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once '../bootstrap.php';
+
+$loader = new Twig_Loader_Filesystem('../template/');
+$twig = new Twig_Environment($loader);
+
+$template = $twig->load('base.html');
+echo $template->render();
