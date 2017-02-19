f34f5c184e2fd70035d2efc25061d70f9428e2e4
Victor Souza <victor_souzas@yahoo.com.br>
2017-02-19 14:04:13 -0300

    Integer

--- public/Test/InsertTest.php
+++ public/Test/InsertTest.php
@@ -1,11 +1,11 @@
 <?php
 use \DataBase\Insert;
 
-include "../DataBase/Instruction.php";
-include "../DataBase/Insert.php";
+include "../../DataBase/Instruction.php";
+include "../../DataBase/Insert.php";
 $insert = new Insert();
 $insert->setEntity("usuarios");
 $insert->setConlumnAsValue('nome', "Victor de Mattos Souza");
-$insert->setConlumnAsValue("idade", "28");
+$insert->setConlumnAsValue("idade", 28);
 $insert->setConlumnAsValue("Sexo", "Masculino");
 echo $insert->getInstruction();
